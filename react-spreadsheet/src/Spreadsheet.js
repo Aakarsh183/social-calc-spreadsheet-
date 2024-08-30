da845fb81101ba2427834a9f6364d100bd0fc91a
Aakarsh183 <geraaakarsh7@gmail.com>
2024-08-31 02:05:13 +0530

    Update Spreadsheet.js

--- react-spreadsheet/src/Spreadsheet.js
+++ react-spreadsheet/src/Spreadsheet.js
@@ -35,21 +35,50 @@ const Spreadsheet = () => {
             handleCellChange(rowIndex, colIndex, result);
         }
     };
+     const sortColumn = (colIndex) => {
+        const sortedData = [...data].sort((a, b) => {
+            if (a[colIndex] < b[colIndex]) return -1;
+            if (a[colIndex] > b[colIndex]) return 1;
+            return 0;
+        });
+        setData(sortedData);
+    };
 
+
+    const sortRow = (rowIndex) => {
+        const sortedData = data[0].map((_, colIndex) =>
+            data.map((row) => row[colIndex])
+        ).sort((a, b) => {
+            if (a[rowIndex] < b[rowIndex]) return -1;
+            if (a[rowIndex] > b[rowIndex]) return 1;
+            return 0;
+        });
+
+        const newData = sortedData[0].map((_, colIndex) =>
+            sortedData.map((row) => row[colIndex])
+        );
+        setData(newData);
+    };
     return (
         <table className="spreadsheet">
             <thead>
                 <tr>
                     <th></th>
                     {data[0].map((_, colIndex) => (
-                        <th key={colIndex}>{String.fromCharCode(65 + colIndex)}</th>
+                        <th key={colIndex}>
+                        {String.fromCharCode(65 + colIndex)}
+                         <button onClick={() => sortColumn(colIndex)}>Sort Col</button>
+                        </th>
                     ))}
                 </tr>
             </thead>
             <tbody>
                 {data.map((row, rowIndex) => (
                     <tr key={rowIndex}>
-                        <th>{rowIndex + 1}</th>
+                        <th>
+                        {rowIndex + 1}
+                        <button onClick={() => sortRow(rowIndex)}>Sort Row</button>
+                        </th>
                         {row.map((cell, colIndex) => (
                             <td
                                 key={colIndex}
